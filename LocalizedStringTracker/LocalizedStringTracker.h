60625b39d22f8daa9452338a81ae3cb2f2acedae
saix <sai.xu@citrix.com>
2017-02-08 15:50:59 +0800

    Initial Commit

---
+++ LocalizedStringTracker/LocalizedStringTracker.h
@@ -0,0 +1,19 @@
+//
+//  LocalizedStringTracker.h
+//  LocalizedStringTracker
+//
+//  Created by saix on 2017/2/8.
+//  Copyright © 2017年 orcman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LocalizedStringTracker.
+FOUNDATION_EXPORT double LocalizedStringTrackerVersionNumber;
+
+//! Project version string for LocalizedStringTracker.
+FOUNDATION_EXPORT const unsigned char LocalizedStringTrackerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocalizedStringTracker/PublicHeader.h>
+
+
